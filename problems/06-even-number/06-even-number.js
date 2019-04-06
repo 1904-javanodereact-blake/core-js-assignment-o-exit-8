3a19f424e70868c5e3c70eb0cf615cda7a4118aa
o-exit-8 <o.exit.8@gmail.com>
2019-04-06 16:25:16 -0500

    Pushing even Number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,18 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    
+
+    someNum /=2;
+    console.log(someNum);
+    let stringNum = someNum.toString();
+    //check if somenumber has a decimal result
+    if (stringNum.includes(".") === true)
+        return false
+    else
+        return true;
 }
+
+console.log(`is this a even number: 
+${isEven(99)}
+and this one: ${isEven(2)} `);
