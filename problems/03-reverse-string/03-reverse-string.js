6ab3901c05c6e1340b7c94a71b0545dbe91ca8d9
o-exit-8 <o.exit.8@gmail.com>
2019-04-06 19:09:46 -0500

    reverseString finished

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,16 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+    let reverseStr='';
+    reverseStr.length = someStr.length;
+  for(let i=0; i<=someStr.length-1; i++)
+  {
+    let char = someStr[(someStr.length-1)-i];
+    reverseStr += char;
+    
+    //push(someStr[i]);
+  }
+  return reverseStr;
 }
+
+console.log(reverseStr("jehudi"));
